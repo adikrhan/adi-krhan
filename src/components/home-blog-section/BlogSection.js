ddf0bc0210ae634d489ce5d7a780a8f717bd45b4
Adi Krhan <adikrhan43@gmail.com>
2022-03-25 10:34:06 +0100

    Minor changes

--- src/components/home-blog-section/BlogSection.js
+++ src/components/home-blog-section/BlogSection.js
@@ -7,27 +7,6 @@ import ClipLoader from "react-spinners/ClipLoader";
 import Butter from "buttercms";
 const butter = Butter("436a6c093ecc4ae2554bb1bffc61f1dc77a011da");
 
-const BLOG_DUMMY = [
-  {
-    src: "https://source.unsplash.com/ljhCEaHYWJ8",
-    alt: "Lisbon",
-    headline: "My trip to Lisbon",
-    text: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-  },
-  {
-    src: "https://source.unsplash.com/jFCViYFYcus",
-    alt: "Forest",
-    headline: "Out in the wild",
-    text: "Last weekend I spent a day out in the wild. I got hunted by a black bear but luckily I could use my Jiu Jitsu skills on him.",
-  },
-  {
-    src: "https://source.unsplash.com/hC_796Wu-VY",
-    alt: "Work",
-    headline: "Hard work work",
-    text: "This work is really hard. Sitting on a train and coding my website and looking out through the window no the passing landscape.",
-  },
-];
-
 const BlogSection = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
@@ -42,7 +21,6 @@ const BlogSection = () => {
         page_size: 3,
       });
       setData(response.data);
-      console.log(data, response);
     } catch (e) {
       setError(`There was an error: ${e.message}`);
     }
